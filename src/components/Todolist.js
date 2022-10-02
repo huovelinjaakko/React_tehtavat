d957b599eea073c595cf5490348e34ea88d6d398
huovelinjaakko <jaakkohuovelin@gmail.com>
2022-10-03 01:35:10 +0300

    Attempting to add Delete buttons

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Todolist(props) {
+
+    const [id,setId] = useState('');
+
+    const deleteFromList = (event) => {
+        
+        setId(event.currentTarget.id);
+        console.log(id);
+        props.todos.filter((todo, i) => todo !== id);
+        // En saanut tätä toimimaan 
+        console.log(props.todos);
+    
+      }
+
     return(
         <div>
             <table>
-                <tbody>
                 <th>Description</th>
                 <th>Date</th>
+                <tbody>
                 {
                 props.todos.map((todo, index) =>
                     <tr key={index}>
                     <td>{todo.desc}</td>
                     <td>{todo.date}</td>
+                    <td><button id={index} type="button" onClick={deleteFromList}>Delete</button></td>
                     </tr>
                 )
                 }
